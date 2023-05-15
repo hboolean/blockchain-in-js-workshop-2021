ffeac278f3fe7ca462a7676e18081384c315677c
hboolean <1007504655@qq.com>
2023-05-15 22:25:33 +0800

    update README.md
    update lesson 1

--- src/models/Block.js
+++ src/models/Block.js
@@ -3,7 +3,12 @@ class Block {
   /* 构造函数需要包含
 
   */
-  constructor() {}
+  constructor(blockchain,parentHash,nonce = sha256(new Date)) {
+    this.blockchain = blockchain;
+    this.nonce = nonce;
+    this.parentHash = parentHash;
+    this.hash=sha256(this.nonce+this.parentHash).toString()
+  }
 }
 
 export default Block
